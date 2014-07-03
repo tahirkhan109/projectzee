c9f2bf8ab241fc31985efc58cb78f651ccd4f483
tahirkhan109 <tahir_lodhi109@yahoo.com>
2014-07-03 23:48:54 +0500

    jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery.datetimepicker
+//= require jquery.dataTables.min
+//= require jquery.cleditor.min
 //= require_tree .
