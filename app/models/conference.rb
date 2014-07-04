5bac3c6a0cfcf968b20741874653f0466c14254e
tahirkhan109 <tahir_lodhi109@yahoo.com>
2014-07-04 18:05:21 +0500

    add validations

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -5,4 +5,13 @@ class Conference < ActiveRecord::Base
   validates :start_datetime, presence: true
   validates :end_datetime, presence: true
   belongs_to :user
+  validate :date_validation
+  def date_validation
+    if self[:end_datetime] < self[:start_datetime]
+      errors[:end_datetime] << "End Date Should Be Greater Then Start Date"
+      return false
+    else
+      return true
+    end
+  end
 end
