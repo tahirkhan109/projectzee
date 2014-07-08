3ed3a2c2a06f10e10a4c37a9f7b87dbad6caae95
tahirkhan109 <tahir_lodhi109@yahoo.com>
2014-07-08 21:14:33 +0500

    add puts

--- app/controllers/attendee_details_controller.rb
+++ app/controllers/attendee_details_controller.rb
@@ -4,6 +4,9 @@ class AttendeeDetailsController < ApplicationController
 
   def check_user
     @attendee = AttendeeDetail.find_by_id(session[:attendee_id])
+    puts "33333333333333333333333333333333",@attendee.id
+    puts "44444444444444444444444444444444",params[:id]
+
     puts "CCC", @attendee.id, params[:id].to_i
     if @attendee.id == params[:id].to_i
 
@@ -46,7 +49,9 @@ class AttendeeDetailsController < ApplicationController
   end
 
   def display_user_data
+    puts "000000000000000000000000000000000",params.inspect
     @attendee = AttendeeDetail.where(:first_name => params[:attendee][:firstname].capitalize, :last_name => params[:attendee][:lastname].capitalize).first
+    puts "111111111111111111111111111111111",@attendee.inspect
     if @attendee.present?
       session[:attendee_id] = @attendee.id
       redirect_to "/attendee_details/attendee_home?id=#{@attendee.id}"
@@ -54,6 +59,7 @@ class AttendeeDetailsController < ApplicationController
       flash[:error] = 'No Information Against This User'
       redirect_to "/"
     end
+    puts "222222222222222222222222222222222",session[:attendee_id].inspect
   end
 end
 private
