4ca2e72c03184665ba88e8216d2b5af8643c9f5c
tahirkhan109 <tahir_lodhi109@yahoo.com>
2014-07-08 22:36:47 +0500

    add checks on live

--- app/controllers/attendee_details_controller.rb
+++ app/controllers/attendee_details_controller.rb
@@ -1,12 +1,9 @@
 class AttendeeDetailsController < ApplicationController
 
-  #before_filter :check_user, :only => [:attendee_flight_detail, :attendee_ground_detail, :attendee_agenda_detail,:attendee_home]
+  before_filter :check_user, :only => [:attendee_flight_detail, :attendee_ground_detail, :attendee_agenda_detail,:attendee_home]
 
   def check_user
     @attendee = AttendeeDetail.find_by_id(session[:attendee_id])
-    puts "33333333333333333333333333333333",@attendee.id
-    puts "44444444444444444444444444444444",params[:id]
-
     puts "CCC", @attendee.id, params[:id].to_i
     if @attendee.id == params[:id].to_i
 
@@ -49,11 +46,7 @@ class AttendeeDetailsController < ApplicationController
   end
 
   def display_user_data
-    puts "000000000000000000000000000000000",params.inspect
-    puts "000000000000000000000000000000000",params[:attendee][:firstname].upcase
-    puts "000000000000000000000000000000000",params[:attendee][:lastname].upcase
     @attendee = AttendeeDetail.where(:first_name => params[:attendee][:firstname].upcase, :last_name => params[:attendee][:lastname].upcase).first
-    puts "111111111111111111111111111111111",@attendee.inspect
     if @attendee.present?
       session[:attendee_id] = @attendee.id
       redirect_to "/attendee_details/attendee_home?id=#{@attendee.id}"
@@ -61,7 +54,6 @@ class AttendeeDetailsController < ApplicationController
       flash[:error] = 'No Information Against This User'
       redirect_to "/"
     end
-    puts "222222222222222222222222222222222",session[:attendee_id].inspect
   end
 end
 private
