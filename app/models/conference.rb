ec686368115bb12ac7159518e27dc05afa84e54a
tahirkhan109 <tahir_lodhi109@yahoo.com>
2014-07-18 12:50:43 +0500

    remove conference check

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -6,15 +6,15 @@ class Conference < ActiveRecord::Base
   validates :start_datetime, presence: true
   validates :end_datetime, presence: true
   belongs_to :user
-  validate :date_validation
-  def date_validation
-    if self[:end_datetime] < self[:start_datetime]
-      errors[:end_datetime] << "Should Be Greater Then Start Datetime"
-      return false
-    else
-      return true
-    end
-  end
+  #validate :date_validation
+  #def date_validation
+  #  if self[:end_datetime] < self[:start_datetime]
+  #    errors[:end_datetime] << "Should Be Greater Then Start Datetime"
+  #    return false
+  #  else
+  #    return true
+  #  end
+  #end
   before_save :uppercase_title
 
     def uppercase_title
